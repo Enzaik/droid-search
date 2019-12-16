6afdc767561d7b8007003a9d7412b9e833ccf8c9
enzaik <imernestodiaz@gmail.com>
2019-12-16 08:20:11 +0100

    fix typo

--- src/components/Roms/Rom/Rom.js
+++ src/components/Roms/Rom/Rom.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles(theme => ({
 
 }));
 
-export default function Roms({ name, description }) {
+export default function Rom({ name, description }) {
   const classes = useStyles();
 
   return (
